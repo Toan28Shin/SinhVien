7f2c1975e2b67edbccd5a734e36cf2efedbedfbc
Toan28Shin <toannqph49664@gmail.com>
2025-03-14 11:17:25 +0700

    SinhVien

---
+++ src/main/java/org/example/quanlysinhvien/QuanLysinhVienApplication.java
@@ -0,0 +1,13 @@
+package org.example.quanlysinhvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLysinhVienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLysinhVienApplication.class, args);
+    }
+
+}
