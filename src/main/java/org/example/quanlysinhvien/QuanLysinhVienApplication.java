9b71430c7f553c39c9a0c3abb183b44dfff6dbe2
Toan28Shin <toannqph49664@gmail.com>
2025-03-14 12:19:03 +0700

    Update QuanLysinhVienApplication.java
    update

--- src/main/java/org/example/quanlysinhvien/QuanLysinhVienApplication.java
+++ src/main/java/org/example/quanlysinhvien/QuanLysinhVienApplication.java
@@ -8,6 +8,7 @@ public class QuanLysinhVienApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuanLysinhVienApplication.class, args);
+        System.out.println("Loading....");
     }
 
 }
